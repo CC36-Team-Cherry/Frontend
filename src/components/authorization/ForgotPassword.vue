4affe7512e6bb1f4f8c82738cc90d486b945577d
Jason Sagalow <jrssagalow@gmail.com>
2024-12-03 18:55:49 +0900

    feat: lang buttons in initial pages

--- src/components/authorization/ForgotPassword.vue
+++ src/components/authorization/ForgotPassword.vue
@@ -1,72 +1,84 @@
 <template>
-    <LoopingRhombusesSpinner v-if="isLoading" class="bg-gray-100"/>
-    <div v-else class="flex justify-center items-center h-screen bg-gray-100">
+  <LoopingRhombusesSpinner v-if="isLoading" class="bg-gray-100" />
+  <div v-else class="flex flex-col h-screen bg-gray-100">
+    <!-- Language Buttons -->
+    <div class="absolute top-4 right-4 flex space-x-2">
+      <button @click="switchLanguage('en-US')"
+        :class="locale === 'en-US' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'"
+        class="py-1 px-3 rounded hover:bg-blue-600 transition duration-200">
+        {{ $t('language.en') }}
+      </button>
+      <button @click="switchLanguage('ja-JP')"
+        :class="locale === 'ja-JP' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'"
+        class="py-1 px-3 rounded hover:bg-blue-600 transition duration-200">
+        {{ $t('language.jp') }}
+      </button>
+    </div>
+
+    <!-- Reset Password Form -->
+    <div class="flex justify-center items-center flex-1">
       <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 class="text-2xl font-bold mb-4">Reset Password</h2>
         <form @submit.prevent="handleSubmit" class="flex flex-col space-y-4">
           <div>
             <label for="username" class="block text-gray-700">
-            Enter your email address:
+              Enter your email address:
             </label>
-            <input
-              type="text"
-              id="username"
-              v-model="email"
-              class="w-full p-2 border border-gray-300 rounded"
-              required
-            />
+            <input type="text" id="username" v-model="email"
+              class="w-full p-2 border border-gray-300 rounded" required />
           </div>
-          <button
-            type="submit"
-            class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-          >
-          Send
+          <button type="submit"
+            class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+            Send
           </button>
-          <button
-            @click="goToLogin()"
-            class="italic underline"
-          >
-          Cancel
+          <button @click="goToLogin()" class="italic underline">
+            Cancel
           </button>
         </form>
       </div>
     </div>
-  </template>
-  
-  
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router'
-  import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
-  import { auth } from '../../firebase/firebaseConfig.ts'
-  import axios from "axios";
-  import LoopingRhombusesSpinner from '../../modal/Loading.vue';
-  axios.defaults.withCredentials = true;
-  
-  const email = ref('')
-  const router = useRouter();
-  const isLoading = ref(false);
-  
-  const handleSubmit = async () => {
-    isLoading.value = true;
-    await resetEmail();
-    router.push({ path: `/login` });
-    isLoading.value = false;
+  </div>
+</template>
 
-    //TODO: add toast popup on success
 
-  };
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router'
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/firebaseConfig.ts'
+import { useI18n } from 'vue-i18n';
+import axios from "axios";
+import LoopingRhombusesSpinner from '../../modal/Loading.vue';
+axios.defaults.withCredentials = true;
 
-  const resetEmail = async () => {
-    try {
-        await sendPasswordResetEmail(auth, email.value);
-    } catch (err) {
-        console.error(err);
-    }
+const email = ref('')
+const router = useRouter();
+const { locale } = useI18n();
+const isLoading = ref(false);
+
+const handleSubmit = async () => {
+  isLoading.value = true;
+  await resetEmail();
+  router.push({ path: `/login` });
+  isLoading.value = false;
+
+  //TODO: add toast popup on success
+
+};
+
+const resetEmail = async () => {
+  try {
+    await sendPasswordResetEmail(auth, email.value);
+  } catch (err) {
+    console.error(err);
   }
-  
-  const goToLogin = () => {
-    router.push({ path: `/login` });
-  }
-  
-  </script>
+}
+
+const goToLogin = () => {
+  router.push({ path: `/login` });
+}
+
+const switchLanguage = (lang) => {
+  locale.value = lang;
+};
+</script>
--- src/components/authorization/LoginForm.vue
+++ src/components/authorization/LoginForm.vue
@@ -1,52 +1,47 @@
 <template>
-  <LoopingRhombusesSpinner v-if="isLoading" class="bg-gray-100"/>
-  <div v-else class="flex justify-center items-center h-screen bg-gray-100">
-    <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
-      <h2 class="text-2xl font-bold mb-4">{{ $t('login.title') }}</h2>
-      <form @submit.prevent="handleLogin" class="flex flex-col space-y-4">
-        <div>
-          <label for="username" class="block text-gray-700">
-            {{ $t('login.email') }}:
-          </label>
-          <input
-            type="text"
-            id="username"
-            v-model="email"
-            class="w-full p-2 border border-gray-300 rounded"
-            required
-          />
-        </div>
-        <div>
-          <label for="password" class="block text-gray-700">
-            {{ $t('login.password') }}:
-          </label>
-          <input
-            type="password"
-            id="password"
-            v-model="password"
-            class="w-full p-2 border border-gray-300 rounded"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
-        >
-          {{ $t('login.submit') }}
-        </button>
-        <button
-          @click="goToRegister()"
-          class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600"
-        >
-          {{ $t('login.register') }}
-        </button>
-        <button
-          @click="goToForgotPassword()"
-          class="italic underline"
-        >
-        Forgot your password?
-        </button>
-      </form>
+  <LoopingRhombusesSpinner v-if="isLoading" class="bg-gray-100" />
+  <div v-else class="flex flex-col h-screen bg-gray-100">
+    <div class="absolute top-4 right-4 flex space-x-2">
+      <button @click="switchLanguage('en-US')"
+        :class="locale === 'en-US' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'"
+        class="py-1 px-3 rounded hover:bg-blue-600 transition duration-200">
+        {{ $t('language.en') }}
+      </button>
+      <button @click="switchLanguage('ja-JP')"
+        :class="locale === 'ja-JP' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700'"
+        class="py-1 px-3 rounded hover:bg-blue-600 transition duration-200">
+        {{ $t('language.jp') }}
+      </button>
+    </div>
+    <div class="flex justify-center items-center flex-1">
+      <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h2 class="text-2xl font-bold mb-4">{{ $t('login.title') }}</h2>
+        <form @submit.prevent="handleLogin" class="flex flex-col space-y-4">
+          <div>
+            <label for="username" class="block text-gray-700">
+              {{ $t('login.email') }}:
+            </label>
+            <input type="text" id="username" v-model="email" class="w-full p-2 border border-gray-300 rounded"
+              required />
+          </div>
+          <div>
+            <label for="password" class="block text-gray-700">
+              {{ $t('login.password') }}:
+            </label>
+            <input type="password" id="password" v-model="password" class="w-full p-2 border border-gray-300 rounded"
+              required />
+          </div>
+          <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+            {{ $t('login.submit') }}
+          </button>
+          <button @click="goToRegister()" class="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
+            {{ $t('login.register') }}
+          </button>
+          <button @click="goToForgotPassword()" class="italic underline">
+            Forgot your password?
+          </button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -58,6 +53,7 @@ import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/authStore';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../firebase/firebaseConfig.ts'
+import { useI18n } from 'vue-i18n';
 import axios from "axios";
 import LoopingRhombusesSpinner from '../../modal/Loading.vue';
 
@@ -68,6 +64,7 @@ const email = ref('')
 const password = ref('');
 const authStore = useAuthStore();
 const router = useRouter();
+const { locale } = useI18n();
 const isLoading = ref(false);
 
 const handleLogin = async () => {
@@ -78,7 +75,7 @@ const handleLogin = async () => {
 
 const getUserFromBackend = async (token: string) => {
   try {
-    const backendData = await axios.post(`${apiUrl}/login`, {email: email.value, token: token}, { withCredentials: true });
+    const backendData = await axios.post(`${apiUrl}/login`, { email: email.value, token: token }, { withCredentials: true });
     // store user data in Pinia
     authStore.login(backendData.data)
   } catch (err) {
@@ -88,7 +85,7 @@ const getUserFromBackend = async (token: string) => {
 
 const updateLastLogin = async () => {
   try {
-    const response = await axios.patch(`${apiUrl}/accounts/${authStore.user?.id}`, {last_login: new Date()})
+    const response = await axios.patch(`${apiUrl}/accounts/${authStore.user?.id}`, { last_login: new Date() })
   } catch (err) {
     console.error('Error updating last_login: ', err);
   }
@@ -111,4 +108,7 @@ const loginFirebase = async () => {
   router.push({ path: `/calendar` });
 }
 
+const switchLanguage = (lang) => {
+  locale.value = lang;
+};
 </script>
