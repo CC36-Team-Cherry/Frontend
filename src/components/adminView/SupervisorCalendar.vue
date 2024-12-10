23262c38c97feeccc640e608532b149a69085b07
Davide Ghezzo <davideghezzo.dg@gmail.com>
2024-12-10 11:41:52 +0900

    fix calendar supervisor size

--- src/components/adminView/SupervisorCalendar.vue
+++ src/components/adminView/SupervisorCalendar.vue
@@ -45,7 +45,9 @@
     </div>
 
     <!-- Calendar -->
-    <div ref="calendar" id="calendar" class="h-full"></div>
+    <div class="flex-1 bg-white shadow-sm border border-slate-200 rounded-lg p-4 overflow-hidden" style="height: calc(100vh - 200px);">
+     <div ref="calendar" class="h-full"></div>
+    </div>
   </div>
 </template>
 
