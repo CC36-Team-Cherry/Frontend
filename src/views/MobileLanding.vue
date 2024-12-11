ca26260cbe02412adc4cd37f9a230b573a15c6d4
prestonb91 <prestonb91@gmail.com>
2024-12-11 19:02:45 +0900

    feat: add pics

--- src/views/MobileLanding.vue
+++ src/views/MobileLanding.vue
@@ -48,9 +48,10 @@
             <!-- 2x2 Grid -->
             <div class="grid grid-cols-2 gap-4 w-full">
                 <!-- First Person -->
-                <div class="flex flex-col items-center">
-                    <div class="flex space-x-2 mt-2">
-                        <a href="https://github.com/prestonb91" class="w-8 h-8">
+                <div class="flex flex-col items-center justify-center">
+                    <div class="flex flex-row items-center space-x-2 mt-2">
+                        <img src="/src/assets/TeamBreeze_Preston.png" alt="Preston Headshot" class="w-14 rounded-xl">
+                        <a href="https://github.com/prestonb91" class="w-10 h-10">
                             <img src="/src/assets/github_logo.png" alt="Logo 1" class="w-full h-full object-contain">
                         </a>
                         <a href="https://www.linkedin.com/in/preston-boardman-8210a671/" class="w-8 h-8">
@@ -62,7 +63,8 @@
 
                 <!-- Second Person -->
                 <div class="flex flex-col items-center">
-                    <div class="flex space-x-2 mt-2">
+                    <div class="flex flex-row items-center space-x-2 mt-2">
+                        <img src="/src/assets/TeamBreeze_Jackson.png" alt="Jackson Headshot" class="w-14 rounded-xl">
                         <a href="https://github.com/jacksontfm" class="w-8 h-8">
                             <img src="/src/assets/github_logo.png" alt="Logo 1" class="w-full h-full object-contain">
                         </a>
@@ -75,7 +77,8 @@
 
                 <!-- Third Person -->
                 <div class="flex flex-col items-center">
-                    <div class="flex space-x-2 mt-2">
+                    <div class="flex flex-row items-center space-x-2 mt-2">
+                        <img src="/src/assets/TeamBreeze_Davide.png" alt="Davide Headshot" class="w-14 rounded-xl">
                         <a href="https://github.com/Ghezz9" class="w-8 h-8">
                             <img src="/src/assets/github_logo.png" alt="Logo 1" class="w-full h-full object-contain">
                         </a>
@@ -88,7 +91,8 @@
 
                 <!-- Fourth Person -->
                 <div class="flex flex-col items-center">
-                    <div class="flex space-x-2 mt-2">
+                    <div class="flex flex-row items-center space-x-2 mt-2">
+                        <img src="/src/assets/TeamBreeze_Jason.png" alt="Jason Headshot" class="w-14 rounded-xl">
                         <a href="https://github.com/ConwayDecker" class="w-8 h-8">
                             <img src="/src/assets/github_logo.png" alt="Logo 1" class="w-full h-full object-contain">
                         </a>
